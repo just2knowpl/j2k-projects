7935829863c93d060d1b79b8d6577023721eacc0
Sype <sype@just2know.pl>
2018-07-14 16:12:17 +0200

    1.0.0
    config edit

--- config2.php
+++ config2.php
@@ -1,12 +1,12 @@
 <?php
 $config = array(
 
-	'IP' => '',
-	'port' => '',
-	'login' => '',
-	'haslo' => '',
-	'ID' => '',
-	'nazwa' => ''
+	'IP' => '37.228.132.129',
+	'port' => '9987',
+	'login' => 'test1',
+	'haslo' => 'AEmxHw9Z',
+	'ID' => '4',
+	'nazwa' => 'tescik tak o'
 
 
 );
