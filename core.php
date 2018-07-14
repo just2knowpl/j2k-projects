f673a36f07a5fd78b66a8da64b505c34051329d6
Manta20 <xanix34547@gmail.com>
2018-07-14 16:13:48 +0200

    1

--- core.php
+++ core.php
@@ -6,7 +6,11 @@ include 'lib/ts3admin.class.php';
 		$ts3connect->login($config['login'],$config['haslo']);
 		$ts3connect->selectServer($config['ID']);
 		$ts3connect->setName('nazwa');
-		
+		while (1)
+			{
+				$core = $ts3connect->getElement('data',$ts3connect->whoAmI());
+				$ts3connect->clientMove($core['client_id'],id kanału);	
+			}
 		
 		
 		
