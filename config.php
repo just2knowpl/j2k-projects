cdcc25a93e15ea9b7a694976348342a61bee48a6
Sype <sype@just2know.pl>
2018-07-14 16:00:55 +0200

    1.0.0
    test

--- config.php
+++ config.php
@@ -1,4 +1,75 @@
 <?php
-//ELO
+	/********************************
+
+	Config File
+
+	********************************/
+$db['host'] = 'localhost';
+$db['login'] = 'root';
+$db['password'] = 'x';
+$db['database'] = 'gbot';
+
+$config['instance']['1']['connection'] = array
+(
+	/*****************************************************
+
+	 		LOGIN TO TEAMSPEAK3 SERVER 
+
+	******************************************************/
+		
+	//TeamSpeak3 Server IP Adress
+
+		'IP' 			=> '37.228.132.129',
+		
+
+	//TeamSpeak3 Server Query Port
+
+		'query_port'		=> '10011',
+
+
+	//TeamSpeak3 Server Port
+
+		'port' 			=> '9987',
+
+
+	//TeamSpeak3 Server Query Login
+
+		'login' 		=> 'serveradmin',
+
+
+	//TeamSpeak3 Server Query Password
+
+		'password' 		=> 'x',
+
+
+	//Bot Nickname
+
+		'bot_name' 		=> 'GWM Administrator',
+
+
+	//Bot Default Channel
+
+		'default_channel' 	=> '2', 
+
+
+	//Bot interval (in seconds)
+		
+		'interval' => 1,
+
+);
+
+
+
+$config['instance']['1']['logs_system'] = array
+(
+
+
+	'logs' => array
+	(
+		'enabled' => true,  		// true or false (logs system)
+	),
+
+
+);
 
 ?>
--- core.php
+++ core.php
@@ -3,9 +3,9 @@ include 'lib/ts3admin.class.php';
 	
 	$ts3connect = new ts3admin($config['IP'], $config['port']);
 	if ($ts3connect->getElement('success', $ts3connect->connect())){
-		$ts3connect->login($config['testowyBocik'],$config['3GpXRdIq']);
+		$ts3connect->login($config['login'],$config['haslo']);
 		$ts3connect->selectServer($config['ID']);
-		$ts3connect->setName('testowyBocik');
+		$ts3connect->setName('nazwa');
 		
 		
 		
