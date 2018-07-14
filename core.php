a76ca07d7d1d2af1b273d006cc6f47958e805899
Manta20 <xanix34547@gmail.com>
2018-07-14 15:55:40 +0200

    2

--- core.php
+++ core.php
@@ -2,4 +2,13 @@
 include 'lib/ts3admin.class.php';
 	
 	$ts3connect = new ts3admin($config['IP'], $config['port']);
+	if ($ts3connect->getElement('success', $ts3connect->connect())){
+		$ts3connect->login($config['login'],$config['haslo']);
+		$ts3connect->selectServer($config['ID']);
+		$ts3connect->setName('nazwa');
+		
+		
+		
+		
+	}
 ?>
