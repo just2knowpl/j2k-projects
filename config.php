f7a3d6d65b0e6681669bb308ff478d287062c505
Manta20 <xanix34547@gmail.com>
2018-07-23 01:23:47 +0200

    1.0.0

--- config.php
+++ config.php
@@ -1,13 +1,38 @@
 <?php
 $config = array(
-
+	// IPv4 serwera 
 	'IP' => '37.228.132.129',
-	'port' => '10011',
+	// Port query serwera (default:10011)
+	'query' => '10011',
+	// Login query 
 	'login' => 'test1',
+	// Hasło query 
 	'haslo' => 'AEmxHw9Z',
-	'ID' => '9988',
+	// Port serwera 
+	'port' => '9988',
+	// Nazwa bota
 	'nazwa' => 'tescik tak o',
+	// Kanał na którym na znajdować się bot
 	'kanal' => '409',
+	//jakies chuj wie co
+	'iconTest' => '',
+	//pierwsza funkcja true/false
+	'f_1' => 'true',
+	//ID ikony do zmiany
+	'icon_1' => '1347345',
+	//1 ikona
+	'icon_which_1' => '/icon_111111111',
+	//czas po jakim ma zmienic 1 ikona na 2
+	'time_1' => '1',
+	//2 ikona
+	'icon_which_1' => '/icon_222222222',
+	//czas po jakim ma zmineic 2 ikone na 3
+	'time_2' => '1',
+	//3 ikona
+	'icon_which_1' => '/icon_333333333',
+	//czas po jakim ma zmienic 3 ikone na 1(+interval)
+	'time_3' => '1',
+	// opóźnie bota 
 	'interval' => '1'
 );
 ?>
--- core.php
+++ core.php
@@ -1,15 +1,35 @@
 <?php
-include 'lib/ts3admin.class.php';
-include 'config.php';
-	$ts3connect = new ts3admin($config['IP'], $config['port']);
+require_once 'lib/ts3admin.class.php';
+require 'config.php';
+	$ts3connect = new ts3admin($config['IP'], $config['query']);
 	if ($ts3connect->getElement('success', $ts3connect->connect())){
 		$ts3connect->login($config['login'],$config['haslo']);
-		$ts3connect->selectServer($config['ID']);
+		$ts3connect->selectServer($config['port']);
 		$ts3connect->setName('nazwa');
+		$core = $ts3connect->getElement('data',$ts3connect->whoAmI());
+		$ts3connect->clientMove($core['client_id'],$config['kanal']);
+		sleep(2);
+		
 		while (1)
 			{
-				$core = $ts3connect->getElement('data',$ts3connect->whoAmI());
-				$ts3connect->clientMove($core['client_id'],$config['kanal']);	
+				//pobranie ikony rangi czy inny chuj weie do czego to jest. Na chuj? nie wiem xD
+				$icon = $ts3connect->getIconByID($config['iconTest']);
+				//podmienienie ikony, chujowo, ale powinno działać
+				if ($config['f_1'] == true) {
+					$ts3connect->uploadIcon($config['icon_which_1'], $config['icon_1']);
+					sleep($config['time_1']);
+					$ts3connect->uploadIcon($config['icon_which_2'], $config['icon_1']);
+					sleep($config['time_2']);
+					$ts3connect->uploadIcon($config['icon_which_3'], $config['icon_1']);
+					sleep($config['time_3']);
+					sleep($config['interval']);
+				}
+				else
+				{
+					break;
+				}
+				break;
+
 			}
 		
 		
